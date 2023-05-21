15d3b47e2f527be7f0d918cc7a8200977eae7f01
TTDAT25 <dtruog51@gmail.com>
2023-05-21 22:17:38 +0700

    datcuto

--- DAS_Courses_Source/src/main/java/com/DAS/OpenAISpring.java
+++ DAS_Courses_Source/src/main/java/com/DAS/OpenAISpring.java
@@ -6,7 +6,7 @@ import com.AnLa.OpenAI.ChatGPT.Model;
 public class OpenAISpring {
 
 	public static void main(String[] args) {
-		ChatGPT cpgt = new ChatGPT("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");
+		ChatGPT cpgt = new ChatGPT("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");
 		String question = "Tóm tắt: \n"
 				+ "1) Mọi người đều có quyền được học hành. Phải áp dụng chế độ giáo dục miễn phí, ít nhất là ở bậc tiểu học và giáo dục cơ sở. Giáo dục tiểu học là bắt buộc. Giáo dục kỹ thuật và ngành nghề phải mang tính phổ thông, và giáo dục cao học phải theo nguyên tắc công bằng cho bất cứ ai có đủ khả năng.\r\n"
 				+ "2) Giáo dục phải hướng tới mục tiêu giúp con người phát triển đầy đủ nhân cách và thúc đẩy sự tôn trọng đối với các quyền và tự do cơ bản của con người. Giáo dục phải tăng cường sự hiểu biết, lòng vị tha và tình bằng hữu giữa tất cả các dân tộc, các nhóm tôn giáo và chủng tộc, cũng như phải đẩy mạnh các hoạt động của Liên Hợp Quốc vì mục đích gìn giữ hoà bình.\r\n"
