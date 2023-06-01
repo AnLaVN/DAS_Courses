12a467d57c74ec40e0bd290703241b9a1947527c
TTDAT25 <dtruog51@gmail.com>
2023-06-01 20:24:29 +0700

    Insert into tables

--- DAS_Courses_Source/src/main/java/com/DAS/TTS/test.java
+++ DAS_Courses_Source/src/main/java/com/DAS/TTS/test.java
@@ -1,5 +1,9 @@
 package com.DAS.TTS;
 
-public class test {
+import com.AnLa.HASH.SHA256;
 
+public class test {
+	public static void main(String[] args) {
+		System.out.println(SHA256.Encrypt("123456"));
+	}
 }
