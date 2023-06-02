78f4e15ccb3d115b1ca71eae0546892636258e98
AnLaVN <binhan29122003@gmail.com>
2023-06-02 23:45:01 +0700

    Optimize reset password code

--- DAS_Courses_Source/src/main/java/com/DAS/Controller/ResetPass.java
+++ DAS_Courses_Source/src/main/java/com/DAS/Controller/ResetPass.java
@@ -26,7 +26,7 @@ public class ResetPass {
 	@Autowired
 	SinhvienDAO sinhvienDAO;
 	
-	String OTP = "", email = "";
+	private String OTP = "", email = "";
 	
 	@GetMapping
 	public String ResetPassGET() {
@@ -35,12 +35,17 @@ public class ResetPass {
 	
 	@PostMapping
 	public String ResetPassPOST(@RequestParam("txtOTP") String pOTP,@RequestParam("txtEmailRP") String pEmail, @RequestParam("txtCPassRP") String pPassword, Model model) {
-		if(pEmail.equals(email) && pOTP.equals(OTP)) {
-			Sinhvien sv = sinhvienDAO.findByEmail(email);
-			sv.setMatkhau(SHA256.Encrypt(pPassword));
-			sinhvienDAO.save(sv);
+		// Xử lí dữ liệu
+		if(pEmail.equals(email) && pOTP.equals(OTP)) {		// Nếu email và mã OTP trùng khớp
+			Sinhvien sv = sinhvienDAO.findByEmail(email);	// Lấy thông tin sinhvien theo email
+			sv.setMatkhau(SHA256.Encrypt(pPassword));		// Lưu mật khẩu mới đã hash
+			sinhvienDAO.save(sv);							// Lưu thông tin vào csdl
+			
+			// Thông báo qua Log
 			Log.add("ResetPassPOST - Change password for " + sv.getUsername() + " successfully.");
 			return "redirect:/SignIn";
+			
+		// Ngược lại thông báo qua Log	
 		}else model.addAttribute("Toast", true);
 		return "ResetPass";
 	}
@@ -48,25 +53,30 @@ public class ResetPass {
 	@PostMapping("/SendEmail")
 	public String ResetPassOTP(@RequestParam("email") String pEmail) {
 		try {
-			if(sinhvienDAO.existsByEmail(pEmail)) {
-				email = pEmail;
-				OTP = RandomORG.getString(8, true, true, true);
+			if(sinhvienDAO.existsByEmail(pEmail)) {			// Nếu địa chỉ email tồn tại trong csdl
+				// Xử lí dữ liệu
+				email = pEmail;								// Lưu địa chỉ email vào biến tạm
+				OTP = RandomORG.getString(8, true, true, true);	// Lấy mã OTP gồm 8 kí tự chữ thường, hoa, số
 				String  text = "Chúng tôi đã nhận được yêu cầu thay đổi mật khẩu của bạn.<br>"
 								+ "Nếu bạn không gửi yêu cầu này, xin vui lòng bỏ qua email này.<br><br>"
 								+ "Nếu bạn tiếp tục nhận được yêu cầu thay đổi mật khẩu, <br>"
 								+ "điều này đồng nghĩa với việc có ai đó đang cố gắng xâm nhập vào tài khoản của bạn.",
-						content = ALEmail.HTMLTemplate
+						content = ALEmail.HTMLTemplate		// Nạp thông tin email
 									.replaceFirst("USERNAME", ALEmail.getHTMLString(sinhvienDAO.findByEmail(email).getTen()))
 									.replaceFirst("CONTENT", ALEmail.getHTMLString(text).replaceAll("&#60;&#98;&#114;&#62;", "<br>"))
 									.replaceFirst("OTPCODE", OTP)
 									.replaceFirst("WARNING", ALEmail.getHTMLString("Không chia sẻ mã này cho bất kì ai !"))
 									.replaceFirst("THANK", ALEmail.getHTMLString("Trân trọng, đội ngũ DAS_Courses"));
-				ALEmail.ZohoMail.setEmail("Yêu cầu thay đổi mật khẩu", content);
-				ALEmail.ZohoMail.sendEmailTO(email);
+				
+				// Gửi email
+				ALEmail.ZohoMail.setEmail("Yêu cầu thay đổi mật khẩu", content);	// Set thông tin email vào máy chủ SMTP
+				ALEmail.ZohoMail.sendEmailTO(email);								// Gửi email đến địa chỉ của sinhvien
+				
+				// Thông báo qua Log
 				Log.add("ResetPassOTP - Send OTP code for user email: " + email);
 			}
 		} catch (IOException | MessagingException e) {
-			e.printStackTrace();
+			Log.add("ResetPassOTP - Error when try to send email to: " + email + "\n\tError code: " + e.toString());
 		}
 		
 		return "redirect:/";
